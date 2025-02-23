15c4a9f0c3df9292bcd96d76636717276cb6f84f
hoangduong6e <duonghoangoffice@gmail.com>
2025-02-24 05:15:21 +0700

    ok

--- Scripts/MenuEventTraoHongDoatLong.cs
+++ Scripts/MenuEventTraoHongDoatLong.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 
 public class MenuEventTraoHongDoatLong : EventManager
 {
-   public void ParseData(JSONNode json)
-   {
-
+    public void ParseData(JSONNode json)
+    {
+        gameObject.SetActive(true);
     }
     protected override void ABSAwake()
     {
 
     }
     protected override void DiemDanhOk(JSONNode json)
+    {
+
+    }
+    public void BtnHoaNguSac()
+    {
+
+    }   
+    public void BtnHoaHong()
     {
 
     }
